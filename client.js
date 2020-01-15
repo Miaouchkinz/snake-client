519d4e704330873e2c31d550f9186223dd2447c3
Gab Richard <g,elodierichard@gmail.com>
2020-01-15 18:34:46 +0000

    added a message to tell the client when the server is connected as well as sending the server the name of the client

--- client.js
+++ client.js
@@ -10,6 +10,14 @@ const connect = function() {
   });
   // interpret incoming data as text
   conn.setEncoding('utf8');
+  //Message to tell you once you've connected
+  conn.on('connect', function() {
+    console.log('Successfully connected to game server.');
+  });
+  // Send name to server
+  conn.on('connect', function() {
+    conn.write('Name: GAB');
+  });
   // prints "you ded cuz you idle" when the server kicks you
   conn.on('data', (data) => {
     console.log(data);
