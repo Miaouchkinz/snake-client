27d77bf770cdfc0762b8850bb1c059e4be493a44
Gab Richard <g,elodierichard@gmail.com>
2020-01-15 18:18:58 +0000

    moved over the connect function to client file

--- play.js
+++ play.js
@@ -1,22 +1,3 @@
-const net = require('net');
-
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({
-    host: '192.168.0.103',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8');
-  // prints "you ded cuz you idle" when the server kicks you
-  conn.on('data', (data) => {
-    console.log(data);
-  });
-
-  return conn;
-};
-
+const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
